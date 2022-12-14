3db266c65610dc93184536ed6beee7cb56f6acb7
Tommy La <tommyl.dmd@gmail.com>
2022-12-14 09:47:44 -0800

    navbar structure created

--- applemakeshift/src/components/navbar/Navbar.js
+++ applemakeshift/src/components/navbar/Navbar.js
@@ -1,11 +1,42 @@
 import React from 'react'
 
+import Logo from '../images/creativearcadeslogo.png'
+
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
-        <nav class="bg-[#323233] h-11">
-
+        <nav class="bg-[#323233] h-11 m-auto">
+            <div class="hidden justify-between items-center w-full md:flex md:w-auto" id="mobile-menu-2">
+                    <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium items-center m-auto text-stone-300 font-sourcesans">
+                        <li>
+                        <Link to ="/"> 
+                            <img src={Logo} alt="Creative Arcades" title="Creative Arcades" class="h-10"></img>
+                        </Link>
+                        </li>
+                        <li>
+                            <Link to="/" class="block md:hover:text-amber-500"> Home </Link>
+                        </li>
+                        <li>
+                            <Link to="manuals" class="block md:hover:text-amber-500"> Arcades </Link>
+                        </li>
+                        <li>
+                            <Link to="manuals" class="block md:hover:text-amber-500"> Pinball </Link>
+                        </li>
+                        <li>
+                            <Link to="manuals" class="block md:hover:text-amber-500"> Gallery </Link>
+                        </li>
+                        <li>
+                            <Link to="manuals" class="block md:hover:text-amber-500"> Help </Link>
+                        </li>
+                        <li>
+                            <Link to="manuals" class="block md:hover:text-amber-500"> Contact </Link>
+                        </li>
+                        <li>
+                            <Link to="manuals" class="block md:hover:text-amber-500"> Blog </Link>
+                        </li>
+                    </ul>
+                </div>
         </nav>
     );
 }
--- applemakeshift/tailwind.config.js
+++ applemakeshift/tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      'sourcesans': ["'Silkscreen'", 'cursive'],
+    }
   },
   plugins: [],
 }
