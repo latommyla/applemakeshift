e01c78511d22f63f8d54b44f3a7f85b3bf13d456
Tommy La <tommyl.dmd@gmail.com>
2022-12-14 09:20:44 -0800

    fixed tailwindcss not displaying

--- applemakeshift/src/routes/Landing.js
+++ applemakeshift/src/routes/Landing.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
 import Navbar from '../components/navbar/Navbar';
-import Heading from '../components/Heading';
 
-export default function Gamelist() {
+export default function Landing() {
   return (
     <main>
       <Navbar />
-      <Heading />
     </main>
   );
 }
