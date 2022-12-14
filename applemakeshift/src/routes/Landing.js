0e082771b45d542940691a1091788f88cc8e9541
Tommy La <tommyl.dmd@gmail.com>
2022-12-14 09:08:27 -0800

    removed and added

---
+++ applemakeshift/src/routes/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Navbar from '../components/navbar/Navbar';
+import Heading from '../components/Heading';
+
+export default function Gamelist() {
+  return (
+    <main>
+      <Navbar />
+      <Heading />
+    </main>
+  );
+}
