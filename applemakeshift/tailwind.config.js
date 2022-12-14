b2ad0b5231ea00b312aba9f2b7081da5986ec638
Tommy La <tommyl.dmd@gmail.com>
2022-12-14 15:44:01 -0800

    added fadein animation

--- applemakeshift/tailwind.config.js
+++ applemakeshift/tailwind.config.js
@@ -8,6 +8,17 @@ module.exports = {
     fontFamily: {
       'sourcesans': ["'Silkscreen'", 'cursive'],
       'lato': ["'Lato'", 'sans-serif'],
+    },
+    keyframes: {
+      'fade-in': {
+        '0%': {
+          opacity: '0',
+          transforms: 'translateX(10px)'
+        },
+      }
+    },
+    animation: {
+      'fade-in': 'fade-in .7s ease-out'
     }
   },
   plugins: [],
