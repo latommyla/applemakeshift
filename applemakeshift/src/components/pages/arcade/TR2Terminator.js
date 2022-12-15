6019b726dbbaf6cb9029f1e15395f6394f721220
Tommy La <tommyl.dmd@gmail.com>
2022-12-15 14:34:04 -0800

    added buy button

--- applemakeshift/src/components/pages/arcade/TR2Terminator.js
+++ applemakeshift/src/components/pages/arcade/TR2Terminator.js
@@ -9,10 +9,17 @@ export default function TR2Terminator() {
                 <p> Free Shipping to Lower 48 States </p>
             </div>
             <div class="m-auto pt-10 text-center animate-fade-in">
-                <span class="text-orange-400"> New </span>
+                <span class="text-orange-400 text-lg font-bold"> New </span>
                 <p class="font-bold text-5xl mt-4"> TR-2 Terminator </p>
-                <p class="mb-10 font-bold text-xl"> Lightgun Arcade System. </p>
-                <img class="m-auto w-96 h-96" src={ Terminator } />
+                <p class="mb-6 font-bold text-xl"> Lightgun Arcade System. </p>
+                <p> From $8499 </p>
+                <div class="flex pb-6">
+                    <div class="m-auto flex items-center p-2">
+                    <button class="bg-blue-500 hover:bg-blue-400 text-white py-2 px-4 rounded-full"> Buy </button>
+                    <p class="text-center ml-4"> Learn more </p>
+                    </div>
+                </div>
+                <img class="m-auto w-96 h-96" src={Terminator} />
             </div>
         </div>
     );
