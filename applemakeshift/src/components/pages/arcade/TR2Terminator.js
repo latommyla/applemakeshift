cb388e790fe451f4d21afcc216cc689695729e71
Tommy La <tommyl.dmd@gmail.com>
2022-12-15 16:59:55 -0800

    adjusted padding

--- applemakeshift/src/components/pages/arcade/TR2Terminator.js
+++ applemakeshift/src/components/pages/arcade/TR2Terminator.js
@@ -5,7 +5,7 @@ import Terminator from '../../images/products/TR2Terminator.png'
 export default function TR2Terminator() {
     return (
         <div class="w-screen-xl font-lato">
-            <div class="mt-10 text-center bg-gray-100 p-3 animate-fade-down">
+            <div class="mt-6 text-center bg-gray-100 p-3 animate-fade-down">
                 <p> Free Shipping to Lower 48 States </p>
             </div>
             <div class="m-auto pt-10 text-center animate-fade-in">
