32d9e00b48566337846c8355dd7791438cfaca24
Tommy La <tommyl.dmd@gmail.com>
2023-01-10 14:08:00 -0800

    added specific height to carousel

--- applemakeshift/src/components/pages/landing/Carousel.js
+++ applemakeshift/src/components/pages/landing/Carousel.js
@@ -9,23 +9,23 @@ import Image4 from '../../images/Carousel04.jpg'
 
 export default function Carouselbottom() {
     return (
-        <div className="mt-10 mb-10 m-auto max-w-5xl h-96">
+        <div className="mt-10 mb-10 m-auto max-w-5xl sm:h-[34rem]">
             <Carousel slideInterval={5000} leftControl=" " rightControl=" ">
                 <img
                     src={ Image1 }
-                    alt="..."
+                    alt="Virtual Pinball"
                 />
                 <img
                     src={ Image2 }
-                    alt="..."
+                    alt="Racing Arcade"
                 />
                 <img
                     src={ Image3 }
-                    alt="..."
+                    alt="Cocktail Arcades"
                 />
                 <img
                     src={ Image4 }
-                    alt="..."
+                    alt="Gunkit"
                 />
             </Carousel>
         </div>
